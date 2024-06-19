46b5e299deee047c42832ea4a163a54bb94a22ff
Ranojoy Banerjee <ronban.14@gmail.com>
2024-06-19 23:57:53 +0530

    Receiving weather data

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState} from "react";
 import { API_KEY } from "../api_cred";
 
 const Weather = () => {
 
-    let data = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${API_KEY}`)
-    console.log(data);
+    // const [city, setCity] = useState("");
+    const [unit, setUnit] = useState('metric');
+    // const [weather, setWeather] = useState({});
+
+    const locationData = fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Pune&limit=5&appid=${API_KEY}`)
+    locationData.then( res => res.json())
+    .then(result => {
+        // console.log(result[0].lat, result[0].lon);
+        const lat = result[0].lat;
+        const lon = result[0].lon;
+    });
+
+    let data = fetch(`https://api.openweathermap.org/data/2.5/weather?q=Nagpur&appid=${API_KEY}&units=${unit}`)
+    data.then(
+    res => res.json())
+    .then(result => console.log(result))
+
+    
   return (
     <div>Weather</div>
   )
